a0a68437c8a862bf8a6e9a793ca9285e10b7b504
Jose Pernia <joseapc90@gmail.com>
2025-01-30 00:54:20 -0300

    v1.0.1 Loading todo task from an static array

---
+++ TodoList-UIKIT/Todo.swift
@@ -0,0 +1,14 @@
+//
+//  Todo.swift
+//  TodoList-UIKIT
+//
+//  Created by Jose Pernia on 2025-01-29.
+//
+
+import Foundation
+
+
+struct Todo {
+    let title: String
+    let isCompleted: Bool = false
+}
--- TodoList-UIKIT/ViewController.swift
+++ TodoList-UIKIT/ViewController.swift
@@ -7,18 +7,19 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
     
-    
-//    let todos = [
-//        Todo(title: "Make vanilla pudding."),
-//        Todo(title: "Share pudding with friends."),
-//        Todo(title: "Eat it while watching TV"),
-//    ]
+    let todos = [
+        Todo(title: "Make vanilla pudding."),
+        Todo(title: "Share pudding with friends."),
+        Todo(title: "Eat it while watching TV"),
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 }
 
@@ -26,20 +27,19 @@ extension ViewController: UITableViewDataSource {
     
     
     func numberOfSections(in tableView: UITableView) -> Int {
-       return 10
+       return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return todos.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        print(indexPath)
+        let todo = todos[indexPath.row]
         let cell = UITableViewCell()
-        cell.textLabel?.text = "🥳"
+        cell.textLabel?.text = todo.title
         return cell
-
     }
 }
 
